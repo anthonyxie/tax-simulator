fea016ce9b0f387bb2b9b2e65f32608b9a538b4a
Cpreo <68442127+Cpreo@users.noreply.github.com>
2023-11-19 17:26:12 -0800

    first commit

---
+++ Tax Evader/Assets/Scripts/Display.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Display : MonoBehaviour
+{
+
+    //Class: Display
+    // Handles interaction between Button elements and their respectful classes
+    // Gets Input and serves it
+    // Updates Tax percentage bar and risk factor bars according to the NetWorth class
+    //Should purchases in Tax Asset class go through the display class? Or should each asset interact directly with a net worth class?
+
+    // Start is called before the first frame update
+    public Button properties;
+    public Button assets;
+    Button Stocks;
+
+    Button submitIncome;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
