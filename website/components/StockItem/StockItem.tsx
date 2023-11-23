8c2112a4ef8690665f055d30d27ec72c186cd75e
Noor Fakih <nfakih@stanford.edu>
2023-11-23 16:20:43 -0500

    stocks ui update

--- website/components/StockItem/StockItem.tsx
+++ website/components/StockItem/StockItem.tsx
@@ -1,16 +1,14 @@
 'use client';
-import { Button, CSSProperties } from "@mantine/core";
-
+import { Button, Divider } from "@mantine/core";
+import "../../resources/panelstyle.css";
 export default function StockItem({stock}) {
-    const spacer: CSSProperties = {
-        marginBottom: '0.5vh',
-      }
     return (
-        <div>
-            <text>{stock.ticker}</text>
-            <div style={spacer}></div>
+        <div className="stockItemDiv">
+            <Divider variant="dotted" id="stockDivider" my="sm" label={stock.ticker} labelPosition="left" color="taupe" />
+            <text>$</text>
             <text>{stock.price}</text>
-            <Button variant="filled" color="taupe">Buy Stock</Button>
+            <Button variant="filled" color="green">Buy 1</Button>
+            <Button variant="filled" color="red">Sell 1</Button>
         </div>
     )
 }
--- website/components/StockList/StockList.tsx
+++ website/components/StockList/StockList.tsx
@@ -5,7 +5,11 @@ import StockItem from "../StockItem/StockItem";
 export default function StockList({stocksList}) {
     return (
         <div>
-            <text>Stocks: </text>
+            <text>Stocks</text>
+            <div id="stocksHeader">
+                <text>Ticker</text>
+                <text>Value</text>
+            </div>
             {stocksList.map((theStock, index) => (<StockItem stock={theStock} key={index}></StockItem>))}
          </div>
     );
