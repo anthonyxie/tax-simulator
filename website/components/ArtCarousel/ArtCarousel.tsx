8e0969497c9e70d666e2b869b860589fca812b4c
Noor Fakih <nfakih@stanford.edu>
2023-11-24 12:00:24 -0500

    removed maxheight from clinet info bar

--- website/components/ArtCarousel/ArtCarousel.tsx
+++ website/components/ArtCarousel/ArtCarousel.tsx
@@ -11,8 +11,6 @@ export default function ArtCarousel({artsList}: ArtListProps) {
             <text className="panelHeader">Art</text>
             { <Carousel slideSize="70%" height={200} slideGap="sm" controlSize={25} loop withIndicators>
                 <Carousel.Slide>1</Carousel.Slide>
-                <Carousel.Slide>2</Carousel.Slide>
-                <Carousel.Slide>3</Carousel.Slide>
               </Carousel> }
          </div>
     );
