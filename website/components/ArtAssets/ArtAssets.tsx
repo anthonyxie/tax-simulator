d23a93a8aaf2027794229e53f6833299f448c89a
Noor Fakih <nfakih@stanford.edu>
2023-11-26 13:36:30 -0800

    slight issue with showing a bit of the next slide

--- website/components/ArtAssets/ArtAssets.tsx
+++ website/components/ArtAssets/ArtAssets.tsx
@@ -2,38 +2,29 @@ import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import ArtAssetItem from '../ArtAssetItem/ArtAssetItem';
+import { Art } from '@/models/stock';
 import '../../resources/stylesheet.css';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    label: 'Nepo Baby Artwork',
+    imgPath: '/assetsImgs/vase.png',
   },
   {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    label: 'Narry Stight by Paclo Pibasso',
+    imgPath: '/assetsImgs/monet.png',
   },
   {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    label: 'Picture of a dog',
+    imgPath: '/assetsImgs/bunny.png',
   },
 ];
 
@@ -60,20 +51,10 @@ function SwipeableTextMobileStepper({ artsList, editArt, donateArt }: ArtListPro
   };
 
   return (
-    <Box id="artDiv" sx={{ maxWidth: '100%', flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
+    <Box id="artDiv">
+      <div>
+        <text className="panelHeader">{images[activeStep].label}</text>
+      </div>
       <AutoPlaySwipeableViews
         id="artCarousel"
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
