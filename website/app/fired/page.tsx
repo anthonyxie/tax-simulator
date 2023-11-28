c702fb800d386f31d86b035c62da3b8ddd3139d4
ting <tinglin0708@gmail.com>
2023-11-28 12:55:34 -0800

    fixed fired text

--- website/app/fired/page.tsx
+++ website/app/fired/page.tsx
@@ -8,7 +8,7 @@ export default function FiredNarrative() {
     const router = useRouter();
     const [storyIndex, setStoryIndex] = useState(0);
     const [gameStory, setGameStory] = useState([
-        `Once again, Elizabeth has paid more taxes than she originally would have, sans your involvement. 
+        `Elizabeth has paid more taxes than she originally would have, sans your involvement. 
         Enough is enough. Only slightly regretfully, she tells you that you are fired.`,
         `You know it was your poor performance that led to this result. But you think of the endless
         zeros in Elizabeth's accounts, and your parents, who have worked tirelessly for over thirty five years 
