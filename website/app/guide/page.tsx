9400866e50c310dd88c7a367e8729992d89d4596
Noor Fakih <nfakih@stanford.edu>
2023-11-27 22:59:54 -0800

    structure for instructions

--- website/app/guide/page.tsx
+++ website/app/guide/page.tsx
@@ -8,22 +8,28 @@ import React from 'react';
 export default function GuidePage() {
   return (
     <>
-    <div className="flexCol" id="parentDiv">
-      <text>Tax Hero</text>
-      <text>how to play</text>
-      <Link id="startLink" href={{ pathname: '/' }}>BACK</Link>
+    <div id="bottomImgs">
+      <div id="bottomImgsCalc"><Image src="/homeImgs/calculator.png" alt="calculator with 377g on it" /> </div>
+      <div id="bottomImgsThing"><Image src="/homeImgs/thing.png" alt="paper thingy?" /> </div>
     </div>
 
     <div id="overlay">
-      <div id="homeDiv">
-        <div id="heading">
-          <br />
+      <div className="flexCol" id="parentDiv">
+        <div className="flexRow" id="firstDiv">
+            <div id="backBttn">
+              <Link href={{ pathname: '/' }}><text id="backLink">BACK</text></Link>
+            </div>
+            <div className="flexCol" id="guideHeader">
+              <text>Tax Hero</text>
+              <text>Instruction Guide</text>
+            </div>
         </div>
-        <div id="bottomImgs">
-          <div id="bottomImgsCalc"><Image src="/homeImgs/calculator.png" alt="calculator with 377g on it" /> </div>
-          <div id="bottomImgsThing"><Image src="/homeImgs/thing.png" alt="paper thingy?" /> </div>
+        <div id="midDiv">
+          <text>hi</text>
         </div>
-
+        <div id="lastDiv">
+          <text>thank you for playing!</text>
+        </div>
       </div>
     </div>
     </>
