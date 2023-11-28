74c6b2d475a7bf67c7e1c5ab39f5151aece9c228
Noor Fakih <nfakih@stanford.edu>
2023-11-27 17:18:53 -0800

    change

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Image } from '@mantine/core';
 import '../resources/home.css';
 import Link from 'next/link';
 import React from 'react';
@@ -13,15 +14,15 @@ export default function StartPage() {
         <br />
       </div>
       <div id="bottomImgs">
-        <div id="bottomImgsCalc"><img src="/homeImgs/calculator.png" alt="calculator with 377g on it" /> </div>
-        <div id="bottomImgsThing"><img src="/homeImgs/thing.png" alt="paper thingy?" /> </div>
+        <div id="bottomImgsCalc"><Image src="/homeImgs/calculator.png" alt="calculator with 377g on it" /> </div>
+        <div id="bottomImgsThing"><Image src="/homeImgs/thing.png" alt="paper thingy?" /> </div>
       </div>
 
     </div>
     <div id="overlay">
       <h1>Tax Hero</h1>
-      <Link id="startLink" href={{ pathname: "/portfolio", query: { round: 0 } as { round: number } }}>START</Link>
-      <Link id="startLink" href={{ pathname: "/narrative", query: { round: 0 } as { round: number } }}>NARRATIVE</Link>
+      <Link id="startLink" href={{ pathname: '/portfolio', query: { round: 0 } as { round: number } }}>START</Link>
+      <Link id="startLink" href={{ pathname: '/narrative', query: { round: 0 } as { round: number } }}>NARRATIVE</Link>
     </div>
     </>
   );
