64c5d43de2e52e03505e1efe6014be5304250c5e
ting <tinglin0708@gmail.com>
2023-11-27 17:13:55 -0800

    dealt with end state

--- website/app/feedback/page.tsx
+++ website/app/feedback/page.tsx
@@ -11,14 +11,22 @@ export default function FeedbackNarrative() {
     const round: number = parseInt(searchParams.get('round') || '0', 10);
     const liquidFunds = searchParams.get('liquid');
 
-    var displayText = [
+    const displayText = [
         // round 0
         [`You saved Y ${tax} in taxes, and were able to acquire him ${liquidFunds} in new liquid funds. He invested all that money in a crypto presale. He sold all his shares immediately when the coin was released publicly, making a killing. All the retail investors saw the value of their coins plummet to zero soon after.
         He used the liquid funds to buy a helicopter, and give it a custom paint job.It’s black, with bright orange flames going up the sides.
         You really love your job.`],
         // round 1
-        ["round 1 feedback"]
+        ["round 1 feedback"],
+        ["round 2 feedback"],
+        ["round 3 feedback"],
     ]
+
+    const NextRoundLink = round < 3 ? (
+        <Link id="continueButton" href={{ pathname: "/narrative", query: { round: round + 1 } }}>Next</Link>
+    ) : (
+        <Link id="continueButton" href="/ending">Next</Link>
+    );
 
     return (
         <div id="narrativeBackground" className="flexCol">
@@ -27,7 +35,7 @@ export default function FeedbackNarrative() {
                     {displayText[round]}
                 </p>
             </div>
-            <Link id="continueButton" href={{pathname: "/narrative", query: {round: round+1}}} >Next Round</Link>
+            {NextRoundLink}
         </div>
     );
 }
