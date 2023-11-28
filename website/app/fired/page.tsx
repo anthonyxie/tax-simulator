19d48bd53d06c03e0af2ffbf25236bd41153122a
ting <tinglin0708@gmail.com>
2023-11-27 22:23:32 -0800

    fired page

---
+++ website/app/fired/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import '../../resources/stylesheet.css';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function FiredNarrative() {
+    const router = useRouter();
+    const [storyIndex, setStoryIndex] = useState(0);
+    const [gameStory, setGameStory] = useState([
+        `Once again, Y has paid more taxes than he originally would have, sans your involvement. 
+        Enough is enough. Only slightly regretfully, he tells you that you are fired.`,
+        `You know it was your poor performance that led to this result. But you think of the endless
+        zeros in Y's accounts, and your parents, who have worked tirelessly for over thirty five years 
+        and will never see a fraction of that money.`,
+        `Finally, you think back to that faithful day, when Y offered you the job, and all the potential
+        that flashed before your eyes: retiring your parents, buying a house, paying off your
+        student debt. Now, the illusion has crumbled and all that's left are ruins. Was there a way
+        for you to win in the first place?`,
+    ]);
+
+    const handleContinue = () => {
+        if (storyIndex < gameStory.length - 1) {
+            setStoryIndex(storyIndex + 1);
+        } else {
+            // end of the narrative
+            console.log('End of the narrative');
+            router.push('/ending');
+        }
+    };
+
+    return (
+        <div id="narrativeBackground" className="flexCol">
+            <div id="narrativeWrapper">
+                <p id="narrativeText">{gameStory[storyIndex]}</p>
+                <br></br>
+                <img src='/narrativeImgs/fired.jpg' alt="text saying FIRED" />
+            </div>
+            <button id="continueButton" onClick={handleContinue}>
+                Continue
+            </button>
+        </div>
+    );
+}
