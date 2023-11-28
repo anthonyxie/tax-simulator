fc8eed10fa7009640b50b9f6e873d36f560c036b
Noor Fakih <nfakih@stanford.edu>
2023-11-27 23:22:11 -0800

    fixed loading issue for guide

--- website/app/guide/page.tsx
+++ website/app/guide/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+/* eslint-disable max-len */
 import { Image } from '@mantine/core';
 import '../../resources/guide.css';
 import Link from 'next/link';
@@ -8,9 +9,14 @@ import React from 'react';
 export default function GuidePage() {
   return (
     <>
-    <div id="bottomImgs">
-      <div id="bottomImgsCalc"><Image src="/homeImgs/calculator.png" alt="calculator with 377g on it" /> </div>
-      <div id="bottomImgsThing"><Image src="/homeImgs/thing.png" alt="paper thingy?" /> </div>
+    <div id="homeDiv">
+      <div id="heading">
+        <br />
+      </div>
+      <div id="bottomImgs">
+        <div id="bottomImgsCalc"><Image src="/homeImgs/calculator.png" alt="calculator with 377g on it" /> </div>
+        <div id="bottomImgsThing"><Image src="/homeImgs/thing.png" alt="paper thingy?" /> </div>
+      </div>
     </div>
 
     <div id="overlay">
@@ -20,15 +26,18 @@ export default function GuidePage() {
               <Link href={{ pathname: '/' }}><text id="backLink">BACK</text></Link>
             </div>
             <div className="flexCol" id="guideHeader">
+              <text>About</text>
               <text>Tax Hero</text>
-              <text>Instruction Guide</text>
             </div>
         </div>
-        <div id="midDiv">
-          <text>hi</text>
+        <div className="flexCol" id="midDiv">
+          <text>You are an accountant to a new member of the 1%! a.k.a an extremely rich person.</text>
+          <text>Learn how the average Joe ends up paying more in taxes than the 1% by using the techniques yourself!</text>
+          <text>In each stage you will implement new tax evading strategies for your client. Your goal is for the actual taxes to end up lower than the projected taxes and to create enough liquid funds to satisfy your client.</text>
+          <text>Play too risky? Get audited! Too safe? Get fired!</text>
         </div>
         <div id="lastDiv">
-          <text>thank you for playing!</text>
+          <Link href={{ pathname: '/' }}><text id="backLink">Let&apos;s lower those taxes!</text></Link>
         </div>
       </div>
     </div>
