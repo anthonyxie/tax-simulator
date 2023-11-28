c64491156d7cfa6cc0af25ad3d73236fbf0a4573
Noor Fakih <nfakih@stanford.edu>
2023-11-27 23:22:51 -0800

    fixed about page

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -22,7 +22,7 @@ export default function StartPage() {
       <h1>Tax Hero</h1>
       <Link id="startLink" href={{ pathname: '/portfolio', query: { round: 0 } as { round: number } }}>START</Link>
       <Link id="startLink" href={{ pathname: '/narrative', query: { round: 0 } as { round: number } }}>NARRATIVE</Link>
-      <Link id="startLink" href="/guide" onClick={() => { window.location.href = '/guide'; }}>GUIDE</Link>
+      <Link id="startLink" href="/about" onClick={() => { window.location.href = '/about'; }}>ABOUT</Link>
     </div>
     </>
   );
