0893a93e614ad8d2e748eb21464b326d6e3a5ef3
ting <tinglin0708@gmail.com>
2023-11-27 21:08:23 -0800

    negative feedback in case paid more than initial

--- website/app/feedback/page.tsx
+++ website/app/feedback/page.tsx
@@ -7,19 +7,29 @@ import { useSearchParams } from 'next/navigation';
 
 export default function FeedbackNarrative() {
     const searchParams = useSearchParams();
-    const tax = searchParams.get('amount');
+    const tax = searchParams.get('amount') || '0';
     const round: number = parseInt(searchParams.get('round') || '0', 10);
     const liquidFunds = searchParams.get('liquid');
 
-    const displayText = [
+    const positiveDisplayText = [
         // round 0
         [`You saved Y ${tax} in taxes, and were able to acquire him ${liquidFunds} in new liquid funds. He invested all that money in a crypto presale. He sold all his shares immediately when the coin was released publicly, making a killing. All the retail investors saw the value of their coins plummet to zero soon after.
         He used the liquid funds to buy a helicopter, and give it a custom paint job.It’s black, with bright orange flames going up the sides.
         You really love your job.`],
         // round 1
-        ["round 1 feedback"],
-        ["round 2 feedback"],
-        ["round 3 feedback"],
+        ["round 1 pos feedback"],
+        ["round 2 pos feedback"],
+        ["round 3 pos feedback"],
+    ]
+
+    const negativeDisplayText = [
+        // round 0
+        [`Y had to pay -${tax} more in taxes this year. He does not seem happy about it. He doesn't say much,
+          but the look he gives you tells you that you better do better next year. Or else.`],
+        // round 1
+        ["round 1 neg feedback"],
+        ["round 2 neg feedback"],
+        ["round 3 neg feedback"],
     ]
 
     const NextRoundLink = round < 3 ? (
@@ -32,7 +42,7 @@ export default function FeedbackNarrative() {
         <div id="narrativeBackground" className="flexCol">
             <div id="narrativeWrapper">
                 <p id="narrativeText">
-                    {displayText[round]}
+                    {(parseInt(tax) >> 0) ? positiveDisplayText[round] : negativeDisplayText}
                 </p>
             </div>
             {NextRoundLink}
