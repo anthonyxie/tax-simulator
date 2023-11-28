b1284305d9c1b292c3d6a9b0f9179838fea69b15
Noor Fakih <nfakih@stanford.edu>
2023-11-28 10:25:56 -0800

    final styling

--- website/components/Loan/Loan.tsx
+++ website/components/Loan/Loan.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { Stock } from "@/models/stock";
-import { Button, Checkbox, Divider, NumberInput } from "@mantine/core";
-import CollateralItem from "./CollateralItem";
-import { useEffect, useState } from "react";
+import { Button, NumberInput } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { Stock } from '@/models/stock';
+import CollateralItem from './CollateralItem';
 
 interface loanProps {
     loanAmount: number,
@@ -13,10 +13,10 @@ interface loanProps {
     makeLoan: any,
     totalLoan: any,
 }
-export default function Loan({loanAmount, setLoanAmount, setCollateral, stockList, makeLoan, totalLoan}: loanProps)  {
+// eslint-disable-next-line max-len
+export default function Loan({ loanAmount, setLoanAmount, setCollateral, stockList, makeLoan, totalLoan }: loanProps) {
     const [disabledIndexes, setDisableIndexes] = useState<number[]>([]);
     const [collatVal, setCollatVal] = useState<number>(0);
-
 
     function loan() {
         setCollateral(disabledIndexes);
@@ -24,12 +24,10 @@ export default function Loan({loanAmount, setLoanAmount, setCollateral, stockLis
     }
 
     function setDisabled(index: number, bool: boolean) {
-        console.log(bool);
-        let disIndexes = disabledIndexes.slice();
-        if (index in disIndexes && bool == false) {
+        const disIndexes = disabledIndexes.slice();
+        if (index in disIndexes && bool === false) {
             disIndexes.splice(disIndexes.indexOf(index), 1);
-        }
-        else if (bool) {
+        } else if (bool) {
             disIndexes.push(index);
         }
 
@@ -39,35 +37,35 @@ export default function Loan({loanAmount, setLoanAmount, setCollateral, stockLis
     useEffect(() => {
         let collatValue = 0;
         disabledIndexes.map((ind, index) => {
-            collatValue += stockList[ind].amount * stockList[ind].price
-        })
+            collatValue += stockList[ind].amount * stockList[ind].price;
+        });
         setCollatVal(collatValue);
     }, [disabledIndexes]);
 
     return (
         <div>
             <NumberInput
-            label="How much would you like to loan?"
-            placeholder={"How much would you like to loan?"}
-            min={0}
-            max={1000000}
-            step={10000}
-            defaultValue={0}
-            value={loanAmount}
-            prefix="$"
-            decimalScale={2}
-            onChange={(value) => setLoanAmount(Number(value))}
+              label="How much would you like to loan?"
+              placeholder="How much would you like to loan?"
+              min={0}
+              max={1000000}
+              step={10000}
+              defaultValue={0}
+              value={loanAmount}
+              prefix="$"
+              decimalScale={2}
+              onChange={(value) => setLoanAmount(Number(value))}
             />
-            <br></br>
+            <br />
             <text>Place collateral on your loan:</text>
             {stockList.map((stock, index) => (
-                <CollateralItem key={index} stock={stock} index={index} setDisabled={setDisabled}/>
+                <CollateralItem key={index} stock={stock} index={index} setDisabled={setDisabled} />
             ))}
-            <text>Collateral value: {collatVal}</text>
-            <br></br>
-            <text>Total Amount Loaned: {totalLoan}</text>
-            <br></br>
-            <Button onClick={loan} disabled={loanAmount == 0}>Apply for Loan</Button>
+            <div className="flexRow" id="loanRow">
+                <text>Collateral value: ${collatVal}</text>
+                <text>Total Amount Loaned: ${totalLoan}</text>
+                <Button id="loanBttn" color="copper" onClick={loan} disabled={loanAmount === 0}>Apply for Loan</Button>
+            </div>
         </div>
     );
 }
